34b876b5c7ca647689ff48927e081c7517f4a9db
Veranika.Patseichuk <Veranika.Patseichuk@ventionteams.com>
2025-04-01 15:32:12 +0200

    Fixed.v2

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,13 +1,10 @@
 from django.contrib.auth import get_user_model
 from adrf.viewsets import ModelViewSet
-from rest_framework.response import Response
 from .serializers import UserSerializer
-from swagger.swagger_async_user_view import swagger_user
 
 User = get_user_model()
 
 
-@swagger_user
 class AsyncUserViewSet(ModelViewSet):
     serializer_class = UserSerializer
     queryset = User.objects.all()
