52949f8d567d76191f41091ec1fea213d299006a
VeronikaPoteichuk <nikaapots@gmail.com>
2025-04-16 00:23:28 +0300

    revert

--- llm_interface.py
+++
@@ -1,7 +0,0 @@
-from abc import ABC, abstractmethod
-
-
-class LLMInterface(ABC):
-    @abstractmethod
-    def generate_review(self, content: str, mode: str) -> str:
-        """Generate a review for the given content and mode."""
