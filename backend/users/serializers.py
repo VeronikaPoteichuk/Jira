85146fb76bbfd2bb3adbf065d42985d4e3a4f7d8
VeronikaPoteichuk <nikaapots@gmail.com>
2025-04-23 22:03:28 +0300

    Added the ability to change and delete a user

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -21,15 +21,3 @@ class UserCreateSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         return User.objects.create_user(**validated_data)
-
-
-class UserUpdateSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ["id", "username", "email"]
-
-    def update(self, instance, validated_data):
-        for attr, value in validated_data.items():
-            setattr(instance, attr, value)
-        instance.save()
-        return instance
