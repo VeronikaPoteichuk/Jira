c4494afa48b5e95da39371fbeec19f8f66ec1db6
VeronikaPoteichuk <nikaapots@gmail.com>
2025-05-20 23:31:41 +0400

    Hided secret_key

--- backend/jira/settings.py
+++ backend/jira/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from django.core.exceptions import ImproperlyConfigured
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,8 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = "django-insecure-!wxs1%tcz682o!22sq&4rltb@6uluy5l_qo-1f&n^xp"
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
+if not SECRET_KEY:
+    raise ImproperlyConfigured("DJANGO_SECRET_KEY env variable is not set")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
